2b4183c30579b731e03c74c71e37af309260d8e2
AJIyanu <aderemijoseph@live.com>
2022-12-22 23:49:22 +0100

    fixes

--- 0x01-ES6_promise/2-then.js
+++ 0x01-ES6_promise/2-then.js
@@ -1,9 +1,8 @@
-#!/usr/bin/node
-
-export default function function handleResponseFromAPI(promise) {
-	promise.then (() => {
-		console.log('Got a response from the API');
-		return { status: 200, body: 'success' };
-	})
-		.catch (() => new Error());
+export default function handleResponseFromAPI (promise) {
+  promise
+    .then(() => {
+      console.log('Got a response from the API');
+      return { status: 200, body: 'success' };
+    })
+    .catch(() => new Error());
 }
--- 0x01-ES6_promise/check.js
+++ 0x01-ES6_promise/check.js
@@ -1,8 +1,4 @@
-<<<<<<< HEAD
-import getFullResponseFromAPI from '../1-promise';
-=======
 import getFullResponseFromAPI from './1-promise';
->>>>>>> refs/remotes/origin/main
 
 console.log(getFullResponseFromAPI(true));
 console.log(getFullResponseFromAPI(false));
