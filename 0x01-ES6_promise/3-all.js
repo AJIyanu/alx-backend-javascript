acd15156abd56a6f7dc757bda1caa54373333d1c
AJIyanu <aderemijoseph@live.com>
2022-12-25 00:27:34 +0100

    fix eslit

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -1,4 +1,9 @@
-export default Function handleProfileSignup() {
-  import uploadPhoto from "./utils";
-  import createUser from "./utils";
- }
+import { uploadPhoto, createUser } from './utils';
+
+export default function handleProfileSignup () {
+  return Promise.all([uploadPhoto(), createUser()])
+    .then((result) => {
+      console.log(result[0].body, result[1].firstName, result[1].lastName);
+    })
+    .catch(() => console.log('Signup system offline'));
+}
