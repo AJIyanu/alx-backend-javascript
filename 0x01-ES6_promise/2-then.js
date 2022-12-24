3a59d261b702e91f7992c179b81a6f85da0613fb
AJIyanu <aderemijoseph@live.com>
2022-12-25 00:47:14 +0100

    finally, catch, then

--- 0x01-ES6_promise/2-then.js
+++ 0x01-ES6_promise/2-then.js
@@ -1,8 +1,8 @@
-export default function handleResponseFromAPI (promise) {
+export default function handleResponseFromAPI(promise) {
   promise
     .then(() => {
-      console.log('Got a response from the API');
       return { status: 200, body: 'success' };
     })
-    .catch(() => new Error());
+    .catch(() => { return new Error()})
+    .finally(() => { console.log('Got a response from the API')})
 }
