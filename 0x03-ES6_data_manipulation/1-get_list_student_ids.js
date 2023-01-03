1eaf10779ee04391b5f65c535cb1ae02b8f4b019
AJIyanu <aderemijoseph@live.com>
2023-01-03 15:57:19 +0100

    mapped out ids

---
+++ 0x03-ES6_data_manipulation/1-get_list_student_ids.js
@@ -0,0 +1,9 @@
+export default function getListStudentId (array) {
+  let ids = [];
+  if (Array.isArray(array)) {
+    ids = array.map(function (item) {
+      return item.id;
+    });
+  }
+  return ids;
+}
