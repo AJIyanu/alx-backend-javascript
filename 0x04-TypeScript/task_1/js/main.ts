ce77c4aa43af64845bd3275d924dc68d188f11f9
AJIyanu <aderemijoseph@live.com>
2023-01-07 16:37:44 +0100

    sync

---
+++ 0x04-TypeScript/task_1/js/main.ts
@@ -0,0 +1,13 @@
+interface Teacher {
+	readonly firstName:string,
+	readonly lastName:string,
+	age:number,
+	location:string
+}
+
+const studentsList: Student[] = [
+	{ firstName: "Rose", lastName: "Aderemi", age: 23, location: "Ominira" },
+	{ firstName: "Nora", lastName: "Gwom", age: 21, location: "Foron" }
+];
+
+
