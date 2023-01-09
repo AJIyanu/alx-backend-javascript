8b8510995c60b92d52d306d6b23eee84a026cf31
AJIyanu <aderemijoseph@live.com>
2023-01-09 11:26:48 +0100

    sync

--- 0x04-TypeScript/task_3/js/main.ts
+++ 0x04-TypeScript/task_3/js/main.ts
@@ -1,11 +1,10 @@
 /// <reference types="./crud" />
 import { RowElement, RowID } from "./interface";
 import * as CRUD from "./crud.js";
-import { insertRow } from "./crud";
 
 const row: RowElement {
   firstName = "Guillaume",
   lastName = "Salva"
 }
 
-const newRowID: RowID = insertRow;
+const newRowID: RowID = CRUD.
