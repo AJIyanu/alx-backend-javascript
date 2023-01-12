6be6a94d737e295e63859117a4dff69bdba9223b
AJIyanu <aderemijoseph@live.com>
2023-01-12 14:42:04 +0100

    packageme

--- 0x04-TypeScript/task_4/js/crud.d.ts
+++
@@ -1,7 +0,0 @@
-import { RowID, RowElement } from "./interface";
-
-declare module "./crud" {
-  export function insertRow (row:RowElement) : number;
-  export function deleteRow (rowId: RowID) : void;
-  export function updateRow (rowid: RowID, row:RowElement) : RowID;
-}
