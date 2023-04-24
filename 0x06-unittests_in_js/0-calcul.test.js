78d9b874fc5f2357e611a7604459625e6aa9d4f4
AJIyanu <aderemijoseph@live.com>
2023-04-24 15:47:14 +0100

    muje

--- 0x06-unittests_in_js/0-calcul.test.js
+++ 0x06-unittests_in_js/0-calcul.test.js
@@ -2,15 +2,38 @@ const assert = require('assert');
 
 const calculateNumber = require('./0-calcul');
 
-describe ('calculateNumber', () => {
-  it('should return the sum of two rounded numbers', () =>  {
-    const a = 4.9;
-    const b = 8.3;
-    const result = 13;
+// describe ('calculateNumber', () => {
+//   it('should return the sum of two rounded numbers', () =>  {
+//     const a = 4.9;
+//     const b = 8.3;
+//     const result = 13;
 
-    const calculate = calculateNumber(a, b);
+//     const calculate = calculateNumber(a, b);
 
-    assert.strictEqual(calculate, result);
-  }
-  );
-});
+//     assert.strictEqual(calculate, result);
+//   }
+//   );
+// });
+
+describe('calculateNumber', () => {
+    it('it round the first argument', () => {
+      assert.equal(calculateNumber(1.0, 0), 1);
+      assert.equal(calculateNumber(1.3, 0), 1);
+      assert.equal(calculateNumber(1.7, 0), 2);
+    });
+
+    it('it round the second argument', () => {
+      assert.equal(calculateNumber(0, 1.0), 1);
+      assert.equal(calculateNumber(0, 1.3), 1);
+      assert.equal(calculateNumber(0, 1.7), 2);
+    });
+
+    it('it should return the right number', () => {
+      assert.equal(calculateNumber(1.3, 0), 1);
+      assert.equal(calculateNumber(0, 1.2), 1);
+      assert.equal(calculateNumber(1.3, 1.3), 2);
+      assert.equal(calculateNumber(1.7, 1.2), 3);
+      assert.equal(calculateNumber(1.3, 1.8), 3);
+      assert.equal(calculateNumber(1.6, 1.8), 4);
+    });
+  });
