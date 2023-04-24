e7a43102e5a4f1821742d4da2ad4d5e618786ec4
AJIyanu <aderemijoseph@live.com>
2023-04-24 15:21:49 +0100

    calculateNumber

--- 0x06-unittests_in_js/0-calcul.test.js
+++ 0x06-unittests_in_js/0-calcul.test.js
@@ -1,6 +1,6 @@
 const assert = require('assert');
 
-const calcuateNumber = require('./0-calcul');
+const calculateNumber = require('./0-calcul');
 
 describe ('calculateNumber', () => {
   it('should return the sum of two rounded numbers', () =>  {
@@ -8,7 +8,7 @@ describe ('calculateNumber', () => {
     const b = 8.3;
     const result = 12;
 
-    const calculate = calcuateNumber(a, b);
+    const calculate = calculateNumber(a, b);
 
     assert.strictEqual(calculate, result);
   }
