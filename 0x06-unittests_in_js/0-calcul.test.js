886fc298ef3c678867c6f0bd2cb9855ab5604d38
AJIyanu <aderemijoseph@live.com>
2023-04-24 16:21:20 +0100

    test cases added

--- 0x06-unittests_in_js/0-calcul.test.js
+++ 0x06-unittests_in_js/0-calcul.test.js
@@ -2,18 +2,21 @@ const assert = require('assert');
 
 const calculateNumber = require('./0-calcul');
 
-// describe ('calculateNumber', () => {
-//   it('should return the sum of two rounded numbers', () =>  {
-//     const a = 4.9;
-//     const b = 8.3;
-//     const result = 13;
+describe ('calculateNumber', () => {
+  it('should return the sum of two rounded numbers', () => {
+    assert.equal(calculateNumber(1.2, 3.5), 5);
+    assert.equal(calculateNumber(2.4, 3.4), 5);
+  }
+  );
 
-//     const calculate = calculateNumber(a, b);
+  it('should return either of the number', () => {
+    assert.equal(calculateNumber(0, 3), 3);
+    assert.equal(calculateNumber(5, 0), 5);
+  }
+  );
 
-//     assert.strictEqual(calculate, result);
-//   }
-//   );
-// });
-
-console.log(calculateNumber);
-assert.equal(calculateNumber(2, 5), 7);
+  it('checks for default value', () => {
+    assert.equal(calculateNumber(3), NaN);
+    assert.equal(calculateNumber(6, '2'), NaN);
+  });
+});
