545b36854ff04cef3699e21e6107c62d4426df44
AJIyanu <aderemijoseph@live.com>
2023-04-27 18:10:42 +0100

    changed describe to expect

--- 0x06-unittests_in_js/2-calcul_chai.test.js
+++ 0x06-unittests_in_js/2-calcul_chai.test.js
@@ -1,33 +1,19 @@
 const assert = require('assert');
 
-const calculateNumber = require('./1-calcul');
+const calculateNumber = require('./2-calcul');
 
-describe ('calculateNumber', () => {
-  it('should return sum of rounded numbers based', () => {
-    assert.equal(calculateNumber('SUM', 1.2, 3.5), 5);
-    assert.equal(calculateNumber('SUM', 1.2, 3.5), 5);
-    assert.equal(calculateNumber('SUM', 1.2, 3.5), 5);
-  }
-  );
+expect(calculateNumber('SUM', 1.2, 3.5)).to.equal(5);
+expect(calculateNumber('SUM', 1.2, 3.5)).to.equal(5);
+expect(calculateNumber('SUM', 1.2, 3.5)).to.equal(5);
 
-  it('should return the difference of rounded numbers', () => {
-    assert.equal(calculateNumber('SUBTRACT', 8.4, 3.4), 5);
-    assert.equal(calculateNumber('SUBTRACT', 8.4, 2), 6);
-    assert.equal(calculateNumber('SUBTRACT', 7.8, 0), 8);
-  }
-  );
+expect(calculateNumber('SUBTRACT', 8.4, 3.4)).to.equal(5);
+expect(calculateNumber('SUBTRACT', 8.4, 2)).to.equal(6);
+expect(calculateNumber('SUBTRACT', 7.8, 0)).to.equal(8);
 
-  it('should return the division two of rounded numbers', () => {
-    assert.equal(calculateNumber('DIVIDE', 8.4, 2), 4);
-    assert.equal(calculateNumber('DIVIDE', 2.0, 2.4), 1);
-    assert.equal(calculateNumber('DIVIDE', 7.9, 1.2), 8);
-  }
-  );
+expect(calculateNumber('DIVIDE', 8.4, 2)).to.equal(4);
+expect(calculateNumber('DIVIDE', 2.0, 2.4)).to.equal(1);
+expect(calculateNumber('DIVIDE', 7.9, 1.2)).to.equal(8);
 
-  it('should return error for division with rounded zero', () => {
-    assert.equal(calculateNumber('DIVIDE', 1.4, 0), 'Error');
-    assert.equal(calculateNumber('DIVIDE', 1.4, 0), 'Error');
-    assert.equal(calculateNumber('DIVIDE', 1.4, 0.4), 'Error');
-  }
-  );
-});
+expect(calculateNumber('DIVIDE', 1.4, 0)).to.equal('Error');
+expect(calculateNumber('DIVIDE', 1.4, 0)).to.equal('Error');
+expect(calculateNumber('DIVIDE', 1.4, 0.4)).to.equal('Error');
