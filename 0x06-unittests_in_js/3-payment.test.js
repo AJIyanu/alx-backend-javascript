84d68b8e6c44c68b57a90eaf93c382d555685ed6
AJIyanu <aderemijoseph@live.com>
2023-04-28 12:33:08 +0100

    called

--- 0x06-unittests_in_js/3-payment.test.js
+++ 0x06-unittests_in_js/3-payment.test.js
@@ -8,7 +8,7 @@ describe('sendPaymentRequestToApi', () => {
     const spy = sinon.spy(Utils.calculateNumber);
     sendPaymentRequestToApi(100, 20);
 
-    expect(spy.calledOnce).to.be.true;
+    expect(spy.called).to.be.true;
     expect(spy.calledWith('SUM', 100, 20)).to.be.true;
 
     spy.restore();
