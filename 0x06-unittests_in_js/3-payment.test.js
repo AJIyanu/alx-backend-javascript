ed55454de8c8606a5d9134b9ab5d017c49284465
AJIyanu <aderemijoseph@live.com>
2023-04-28 12:39:17 +0100

    fixes

--- 0x06-unittests_in_js/3-payment.test.js
+++ 0x06-unittests_in_js/3-payment.test.js
@@ -8,7 +8,7 @@ describe('sendPaymentRequestToApi', () => {
     const spy = sinon.spy(Utils.calculateNumber);
     sendPaymentRequestToApi(100, 20);
 
-    expect(sinon.assert.calledWith(spy, 'SUM', 100, 200).to.be.true;
+    expect(sinon.assert.calledWith(spy, 'SUM', 100, 200)).to.be.true;
     expect(spy.calledWith('SUM', 100, 20)).to.be.true;
 
     spy.restore();
