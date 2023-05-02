22aa556c9f8fea853a50f573702da6c6de4fbbcc
AJIyanu <aderemijoseph@live.com>
2023-05-02 18:13:24 +0100

    fixing stub

--- 0x06-unittests_in_js/4-payment.test.js
+++ 0x06-unittests_in_js/4-payment.test.js
@@ -5,14 +5,15 @@ const expect = require('chai').expect;
 
 describe('sendPaymentRequestToApi', () => {
   it('should call the Utils.calculateNumber and return 10', ()=> {
-    const Stub = sinon.stub().returns(10);
-    const consoleStub = sinon.stub(console, 'log');
     const spy = sinon.spy(Utils, 'calculateNumber');
+    const Stub = sinon.stub(Utils, 'calculateNumber').returns(10);
+    const consoleStub = sinon.stub(console, 'log');
     Stub.callsFake(spy);
 
     sendPaymentRequestToApi(100, 20);
 
     expect(spy.calledOnce).to.be.true;
+    expect(Stub.calledOnce).to.be.true;
     expect(spy.calledWith('SUM', 100, 20)).to.be.true;
     expect(consoleStub.calledWith('The total is: 10')).to.be.true;
 
