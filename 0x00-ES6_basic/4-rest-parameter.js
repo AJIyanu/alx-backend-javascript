50e0bd9b672c0e245dd1dc1337077be50b4a1c25
AJIyanu <aderemijoseph@live.com>
2023-09-26 11:36:23 +0100

    es too get wahala

--- 0x00-ES6_basic/4-rest-parameter.js
+++ 0x00-ES6_basic/4-rest-parameter.js
@@ -1,6 +1,10 @@
 export default function returnHowManyArguments(...simi) {
+  let count = 0;
 
   let rest;
-  for (rest of simi);
-  return rest + 1;
+  for (rest of simi) {
+    count += 1;
+    console.error(rest);
+  }
+  return count;
 }
