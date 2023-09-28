694aa66d564ce0eafb5c2cbb2301531be79fead0
Aderemi Joseph <99488361+AJIyanu@users.noreply.github.com>
2023-09-28 10:54:23 +0100

    return apromise

--- 0x01-ES6_promise/0-promise.js
+++ 0x01-ES6_promise/0-promise.js
@@ -1,5 +1,9 @@
 #!/usr/bin/node
 
 export default function getResponseFromAPI() {
-  return Promise.resolve();
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      resolve(); // Simulate a delayed response
+    }, 1000); // Wait for 1 second before resolving
+  });
 }
