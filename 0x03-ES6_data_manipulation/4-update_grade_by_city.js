f8536e11c790539b6b7afefff95d36cb9f45e379
AJIyanu <aderemijoseph@live.com>
2023-10-03 07:37:19 +0100

    sync

--- 0x03-ES6_data_manipulation/4-update_grade_by_city.js
+++ 0x03-ES6_data_manipulation/4-update_grade_by_city.js
@@ -1,3 +1,7 @@
 export default function updateStudentGradeByCity(getListStudents, city, newGrades) {
-    return getListStudents.map();
+  return getListStudents.filter((studObj) => (studObj === city)).map(
+    (locStud) => {
+      funv
+    }
+  );
 }
