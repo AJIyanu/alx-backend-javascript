e2581a22b7abc6e7f3de68fd6ae665990f55bc8e
Aderemi Joseph <99488361+AJIyanu@users.noreply.github.com>
2023-10-04 17:11:35 +0100

    typescript webpack trying to understand

--- 0x04-TypeScript/task_0/js/main.ts
+++ 0x04-TypeScript/task_0/js/main.ts
@@ -1,15 +1,34 @@
 interface Student {
-	firstName:string,
-	lastName:string,
-	age:number,
-	location:string
+	firstName:string;
+	lastName:string;
+	age:number;
+	location:string;
 }
 
-const studentsList: Student[] = [
-	{ firstName: "Rose", lastName: "Aderemi", age: 23, location: "Ominira" },
-	{ firstName: "Nora", lastName: "Gwom", age: 21, location: "Foron" }
-];
 
-for (let element of studentsList) {
-  console.table([element], ['firstName', 'location'])
-}
+const student1: Student = {
+    firstName: "Rose",
+    lastName: "AJ",
+    age: 24,
+    location: "Abuja"
+};
+
+const student2: Student = {
+    firstName: "AJ",
+    lastName: "Iyanu",
+    age: 28,
+    location: "Texas"
+};
+
+const studentsList: Student[] = [student1, student2];
+
+const tableBody = document.getElementById("tableBody");
+
+studentsList.forEach(student => {
+	const row = document.createElement("tr");
+	row.innerHTML = `
+		<td>${student.firstName}</td>
+		<td>${student.location}</td>
+	`;
+	tableBody.appendChild(row);
+});
