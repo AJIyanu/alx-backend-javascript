4f6203b131f8d00a7d5f7fc653259170902648c8
AJIyanu <aderemijoseph@live.com>
2023-10-04 08:41:50 +0100

    symv

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -1,10 +1,11 @@
 export default function cleanSet(set, startString) {
-  if (startString === '') return '';
+  if (! (startString instanceof String)) return '';
   const exist = [];
   set.forEach((stng) => {
+    if (stng instanceof String) {
     if (stng.startsWith(startString)) {
       exist.push(stng.slice(startString.length));
-    }
+    }}
   });
   return exist.join('-');
 }
