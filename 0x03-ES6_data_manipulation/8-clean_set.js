19b855c6a1aa8192f47dd549d89746d7dc3f8ff8
Aderemi Joseph <99488361+AJIyanu@users.noreply.github.com>
2023-10-04 09:41:28 +0100

    muje

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -1,11 +1,12 @@
 export default function cleanSet(set, startString) {
-  if ( typeof startString !== "string" || startString === '') {return '';}
+  if (typeof startString !== 'string' || startString === '') { return ''; }
   const exist = [];
   set.forEach((stng) => {
     if (typeof stng === 'string') {
-    if (stng.startsWith(startString)) {
-      exist.push(stng.slice(startString.length));
-    }}
+      if (stng.startsWith(startString)) {
+        exist.push(stng.slice(startString.length));
+      }
+    }
   });
   return exist.join('-');
 }
