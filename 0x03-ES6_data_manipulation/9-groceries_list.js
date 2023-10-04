335c41d55b88682795c39b3721d6ffff87a2a582
Aderemi Joseph <99488361+AJIyanu@users.noreply.github.com>
2023-10-04 04:00:49 +0100

    map fixes

---
+++ 0x03-ES6_data_manipulation/9-groceries_list.js
@@ -0,0 +1,9 @@
+export default function groceriesList() {
+  const fruitmap = new Map();
+  fruitmap.set('Apples', 10);
+  fruitmap.set('Tomatoes', 10);
+  fruitmap.set('Pasta', 1);
+  fruitmap.set('Rice', 1);
+  fruitmap.set('Banana', 5);
+  return fruitmap;
+}
