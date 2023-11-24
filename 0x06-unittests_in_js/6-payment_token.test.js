3210143df875f05a4753425192178025337ef5d6
AJ Iyanu <aderemijoseph@live.com>
2023-11-24 17:55:39 +0100

    sync

--- 0x06-unittests_in_js/6-payment_token.test.js
+++ 0x06-unittests_in_js/6-payment_token.test.js
@@ -5,8 +5,8 @@ describe ('getPaymentTokenFromAPI', () => {
 	it('it should return Successful response from the API', function(done) {
 		getPaymentTokenFromAPI(true)
 			.then((result) => {
-			console.log(result, result.result);
-			assert.equal(result.result, "Successful response from the API");
+			const check = result.result;
+			assert.equal(result, true);
 			done();
 		});
 		// done();
