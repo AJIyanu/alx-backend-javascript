6e1ece635f296c657ad9fbae309db571d03fc391
AJ Iyanu <aderemijoseph@live.com>
2023-11-24 04:53:52 +0100

    async done

--- 0x06-unittests_in_js/6-payment_token.test.js
+++ 0x06-unittests_in_js/6-payment_token.test.js
@@ -1,3 +1,17 @@
 const getPaymentTokenFromAPI = require('./6-payment_token');
+const assert = require('assert');
 
+describe ('getPaymentTokenFromAPI', () => {
+	it('it should return Successful response from the API', function(done) {
+		getPaymentTokenFromAPI(true)
+			.then((result) => {
+			assert.equal(result.data, "Successful response from the API");
+		});
+		done();
+	});
 
+	it('should do nothing when input is false', (done) => {
+		assert.equal(getPaymentTokenFromAPI(false), undefined);
+		done();
+	});
+});
