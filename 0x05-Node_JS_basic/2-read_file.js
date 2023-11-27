7fbab64d11c4ac388604b1614681fc9eec6da256
AJ Iyanu <aderemijoseph@live.com>
2023-11-27 03:42:05 +0100

    check:

--- 0x05-Node_JS_basic/2-read_file.js
+++ 0x05-Node_JS_basic/2-read_file.js
@@ -1,28 +1,60 @@
 const fs = require('fs');
-const csvPaser = require('csv-parse');
+const { parse } = require('csv-parse');
 
 
 function countStudents(path) {
 	try{
 		var data = fs.readFileSync(path, 'utf8');
-	} catch {
-		throw new Error('Cannot load the database');
-	}
+	} catch 
+	{	throw new Error('Cannot load the database')}
 
 	const parseOpt = {
-		columns: true,
+		delimiter: ",",
+		from_line: 2,
 		skip_empty_lines: true
 	};
-	console.log(data);
 
-//	csvPaser(data, parseOpt, (err, rows) => {
-//		if (err) {
-//			console.error(error);
-//			return;
-//		}
-//		console.log('Number of students: ', rows.length);
-//		console.log(rows);
-//	})
+	parse(data, parseOpt, (err, rows) => {
+		if (err) {
+			console.log(err);
+			return;
+		}
+		console.log('Number of students: ', rows.length);
+		const fielcount = countStudentField(rows);
+		for (const field in fielcount) {
+			process.stdout.write('Number of students in ' + field);
+			process.stdout.write(': ' + fielcount[field]);
+			listStudent(field, rows);
+		}
+	})
+}
+
+function countStudentField (rows) {
+	let studentField = {};
+
+	for (const row of rows) {
+		if (studentField.hasOwnProperty(row[3])) {
+			studentField[row[3]] = studentField[row[3]] + 1
+		} else {
+			studentField[row[3]] = 1
+		}
+	}
+	return studentField;
+}
+
+function listStudent (fieldName, rows) {
+	process.stdout.write('. List:');
+	let check = false;
+	for (const name of rows) {
+		
+		if (fieldName == name[3]) {
+		if (check === true) {
+			process.stdout.write(',');
+		}	process.stdout.write(' ' + name[0]);
+		check = true;
+		}
+	}
+	process.stdout.write('\n')
 }
 
 module.exports = countStudents;
