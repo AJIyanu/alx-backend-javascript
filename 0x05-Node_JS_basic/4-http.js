92fee34c249ac513e606293168f28ed531710111
Aderemi Joseph <99488361+AJIyanu@users.noreply.github.com>
2023-11-29 16:39:33 +0100

    ubuntu test

--- 0x05-Node_JS_basic/4-http.js
+++ 0x05-Node_JS_basic/4-http.js
@@ -1,6 +1,15 @@
 const http = require('http');
 
-http.createServer((req, res) => {
-    res.write('Hello Holberton School!');
-    res.end();
-}).listen(1245)
+
+const host = '127.0.0.1';
+const port = '1245';
+
+
+const app = http.createServer((req, res) => {
+    res.statusCode = 200;
+    res.end('Hello Holberton School!');
+})
+
+app.listen(port, host, () => console.log('Listening...'));
+
+module.exports = app
