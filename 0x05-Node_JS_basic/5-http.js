1ee53275a7faa33764163b433038ff42814ac39f
Aderemi Joseph <99488361+AJIyanu@users.noreply.github.com>
2023-11-30 03:18:43 +0100

    async file reading returned as enpoint

---
+++ 0x05-Node_JS_basic/5-http.js
@@ -0,0 +1,29 @@
+const http = require('http');
+const countStudent = require('./3-read_file_async.js');
+
+const host = "127.0.0.1";;
+const port = "1245";
+
+const app = http.createServer( async (req, res) => {
+    res.statusCode = 200;
+    if (req.url === '/') {
+        res.end('Hello Holberton School!');
+      } else if (req.url === '/students') {
+        let studList = 'This is the list of our students\n';
+        await countStudents(process.argv[2])
+          .then((msg) => {
+            studList += msg;
+            res.end(studList);
+          })
+          .catch((err) => {
+            studList += err.message;
+            res.end(studList);
+          });
+      }
+    });
+
+    app.listen(port, hostname, () => {
+      console.log(`Server running at http://${hostname}:${port}`);
+    });
+
+    module.exports = app;
